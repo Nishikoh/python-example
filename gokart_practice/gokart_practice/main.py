3e12a9eca86668c012a0e0cadd918c2eef3946b0
Koki Nishihara <snkg2450@gmail.com>
2022-02-20 22:49:25 +0900

    update

--- gokart_practice/gokart_practice/main.py
+++ gokart_practice/gokart_practice/main.py
@@ -4,10 +4,22 @@ import time
 
 class Example(gokart.TaskOnKart):
     def run(self):
-        time.sleep(5)
+        time.sleep(10)
         self.dump("Hello, world!")
 
 
-task = Example()
+class StringToSplit(gokart.TaskOnKart):
+    """Like the function to divide received data by spaces."""
+
+    task = gokart.TaskInstanceParameter()
+
+    def run(self):
+        time.sleep(10)
+        sample = self.load("task")
+        self.dump(sample.split(" "))
+
+
+# task = Example()
+task = StringToSplit(task=Example())
 output = gokart.build(task)
 print(output)
